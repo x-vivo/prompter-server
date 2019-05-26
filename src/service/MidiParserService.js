6a4ebb9a453a34adbd511f35a8dfae5cd83519f0
Zedd <>
2019-05-26 10:31:33 +0200

    initial implementation

---
+++ src/service/MidiParserService.js
@@ -0,0 +1,36 @@
+export default class MidiParserService {
+	constructor({onMessage}){
+		this.onMessage = onMessage;
+	}
+	parse(buff){
+		new String(buff)
+		.split('\n')
+		.filter(message => message)
+		.map(message => this.parseMessage(message.split(' ').map(part => parseInt(part, 16))));
+	}
+	parseMessage(message){
+		const byte = message.shift();
+		switch (byte){
+			case 248:
+				this.onMessage({type:'TIMING_CLOCK'});
+				break;
+			case 250:
+				this.onMessage({type:'START'});
+				break;
+			case 251:
+				this.onMessage({type:'CONTINUE'});
+				break;
+			case 252:
+				this.onMessage({type:'STOP'});
+				break;
+			case 254:
+				this.onMessage({type:'ACTIVE_SENSING'});
+				break;
+			case 255:
+				this.onMessage({type:'RESET'});
+				break;
+			default:
+			break;
+		}
+	}
+}
---
+++ src/socketServer.js
@@ -0,0 +1,117 @@
+const ws = require('ws');
+const schedule = require('node-schedule');
+const uuid = require('node-uuid');
+const { spawnSync, spawn } = require('child_process');
+import MidiParserService from './service/MidiParserService';
+
+const PORT = 5050;
+
+class PrompterServer {
+	constructor(){
+		this.clients = {};
+		this.callTimeout = 20000;
+		this.callPingInterval = 10;
+		this.midiParserService = new MidiParserService({
+			onMessage = message => this.processMidi(message)
+		});
+		this.midiListenerProcess;
+		
+		this.webSocketServer = new ws.Server({
+			port: PORT
+		});
+	
+		this.webSocketServer.on('connection', ws => {
+			this.processConnection({
+				socket: ws
+			});
+		});
+	
+		this.jobs = {
+			'PING': schedule.scheduleJob(`*/${this.callPingInterval} * * * * *`, () => {
+				console.log('PING job');
+			})
+		};
+	}
+
+	processConnection(options) {
+		var id = uuid.v1();
+		this.clients[id] = options.socket;
+		options.socket.on('message', message => {
+			message = JSON.parse(message);
+
+			console.log(message.type);
+
+			switch (message.type) {
+				case 'ID':
+					this.processCommandId({
+						socketId: id,
+						message
+					});
+					break;
+				case 'LIST_CONNECTIONS_REQUEST':
+					this.processCommandListConnections({
+						socketId: id,
+						message
+					});
+					break;
+				case 'CONNECT':
+					this.processCommandConnect({
+						socketId: id,
+						message
+					});
+					break;
+				default:
+					console.log('Unexpected message: ', message);
+					break;
+			}
+		});
+		options.socket.on('close', () => {
+			console.log('####################################### socket closed:', id);
+			delete this.clients[id];
+		});
+
+		this.clients[id].send(JSON.stringify({
+			type: 'REQUEST_ID'
+		}));
+
+		Object.values(this.clients).map(client => client.send(JSON.stringify({
+			type: 'CLIENT_LIST',
+			payload: Object.keys(this.clients)
+		})));
+	}
+
+	processCommandId(options) {
+		console.log(options);
+	};
+	processMidi(message){
+		if(message.type === 'TIMING_CLOCK'){
+			return;
+		}
+		Object.values(this.clients).map(client => client.send(JSON.stringify({
+			type: 'MIDI',
+			payload: message
+		})));
+	}
+	processCommandConnect({socketId, message}){
+		this.midiListenerProcess = spawn('amidi', [`-p${message.payload}`, '-d']);
+		this.midiListenerProcess.stdout.on('data', chunk => {
+			this.midiParserService.parse(chunk);
+		});
+	}
+	processCommandListConnections({socketId, message}) {
+		console.log(socketId, message);
+		const child = spawnSync('amidi', ['-l']);
+		const [header, ...connectionsData] = new String(child.stdout).split('\n');
+		const connections = connectionsData.reduce((connections, connectionData) => {
+				const [connectionDir, port, ...data] = connectionData.trim().replace(/ +(?= )/g,'').split(' ');
+				connections[port] = data.join(' ');
+				return connections;
+			}, {});
+		this.clients[socketId].send(JSON.stringify({
+			type: 'LIST_CONNECTIONS_RESPONSE',
+			payload: connections
+		}));
+	};
+};
+
+new PrompterServer();
